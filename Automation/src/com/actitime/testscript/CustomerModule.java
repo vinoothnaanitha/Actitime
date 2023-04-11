15dcd6964248d6a08e96863d8473a1c8551f5946
vinoothnaanitha <119551463+vinoothnaanitha@users.noreply.github.com>
2023-04-11 16:40:54 +0530

    Create CustomerModule.java

---
+++ Automation/src/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,17 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+
+public class customerModule   {
+
+	@Test
+	public void createcustomer() {
+		Reporter.log("createcustomer", true);
+		
+	}
+
+
+}
